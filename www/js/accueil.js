52369a15885b9477573d3cc45eecb98ce7b50cd3
MissYoruwara <cecel31@free.fr>
2018-04-18 17:09:26 +0200

    initial commit

---
+++ www/js/accueil.js
@@ -0,0 +1,11 @@
+var app = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        
+    }
+};
+
+app.initialize();
